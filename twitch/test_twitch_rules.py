7a3d929703fe8685b993393f32149c59deb9d879
Plytha <alandreax@gmail.com>
2025-07-11 18:45:42 +0200

    Unit tests pogs

---
+++ twitch/test_twitch_rules.py
@@ -0,0 +1,154 @@
+import pytest
+from rule import Rule
+
+def build_from_dict(rule_dict):
+    if "ordered" in rule_dict.keys():
+        ordered = rule_dict["ordered"]
+    else:
+        ordered = False
+    rule_object = Rule(rule_dict["name"], 
+                       rule_dict["matches"] == "all", 
+                       ordered,
+                       rule_dict["filters"]["words"],
+                       rule_dict["filters"]["regex"])
+    return rule_object
+
+def test_one_word_rule_fires_on_word_found():
+    rule_dict = {
+        "name": "one word test",
+        "matches": "all",
+        "filters": 
+        {
+            "words": ["test"],
+            "regex": []
+        }
+    }
+
+    rule = build_from_dict(rule_dict)
+    message_string = "there is the word test in me"
+    assert rule.check(message_string)
+
+def test_one_word_rule_does_not_fire_on_word_missing():
+    rule_dict = {
+        "name": "one word test",
+        "matches": "all",
+        "filters": 
+        {
+            "words": ["test"],
+            "regex": []
+        }
+    }
+
+    rule = build_from_dict(rule_dict)
+    message_string = "the word is absent in me"
+    assert not rule.check(message_string)
+
+def test_multiple_word_any_rule_fires_on_words_found():
+    rule_dict = {
+        "name": "multiple words any",
+        "matches": "any",
+        "filters": 
+        {
+            "words": ["red", "blue"],
+            "regex": []
+        }
+    }
+
+    rule = build_from_dict(rule_dict)
+    message_string = "bla bla bla red bla bla"
+    assert rule.check(message_string)
+
+def test_multiple_word_any_rule_does_not_fire_on_words_missing():
+    rule_dict = {
+        "name": "multiple words any",
+        "matches": "any",
+        "filters": 
+        {
+            "words": ["red", "blue"],
+            "regex": []
+        }
+    }
+
+    rule = build_from_dict(rule_dict)
+    message_string = "bla bla bla bla bla"
+    assert not rule.check(message_string)
+
+def test_multiple_word_all_ordered_rule_fires_on_words_found():
+    rule_dict = {
+        "name": "multiple words all ordered",
+        "matches": "all",
+        "ordered": True,
+        "filters": 
+        {
+            "words": ["three", "four"],
+            "regex": []
+        }
+    }
+
+    rule = build_from_dict(rule_dict)
+    message_string = "one two three four five six seven"
+    assert rule.check(message_string)
+
+def test_multiple_word_all_ordered_rule_does_not_fire_on_words_missing():
+    rule_dict = {
+        "name": "multiple words all ordered",
+        "matches": "all",
+        "ordered": True,
+        "filters": 
+        {
+            "words": ["three", "four"],
+            "regex": []
+        }
+    }
+
+    rule = build_from_dict(rule_dict)
+    message_string = "one two three ten five six"
+    assert not rule.check(message_string)
+
+def test_multiple_word_all_ordered_rule_does_not_fire_on_words_unordered():
+    rule_dict = {
+        "name": "multiple words all ordered",
+        "matches": "all",
+        "ordered": True,
+        "filters": 
+        {
+            "words": ["three", "four"],
+            "regex": []
+        }
+    }
+
+    rule = build_from_dict(rule_dict)
+    message_string = "five four three two one"
+    assert not rule.check(message_string)
+
+def test_multiple_word_all_unordered_rule_fires_on_words_found():
+    rule_dict = {
+        "name": "multiple words all unordered",
+        "matches": "all",
+        "ordered": False,
+        "filters": 
+        {
+            "words": ["one", "two"],
+            "regex": []
+        }
+    }
+
+    rule = build_from_dict(rule_dict)
+    message_string = "one two three four five six seven"
+    assert rule.check(message_string)
+
+def test_multiple_word_all_unordered_rule_does_not_fire_on_words_missing():
+    rule_dict = {
+        "name": "multiple words all unordered",
+        "matches": "all",
+        "ordered": False,
+        "filters": 
+        {
+            "words": ["one", "two"],
+            "regex": []
+        }
+    }
+
+    rule = build_from_dict(rule_dict)
+    message_string = "one three ten five six"
+    assert not rule.check(message_string)
